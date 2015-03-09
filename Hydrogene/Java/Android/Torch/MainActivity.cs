5b00ee740a7ea134b35b07f21e3b38b0df303d83
marc hoffman <mh@remobjects.com>
2015-03-09 09:08:39 +0100

    Torch Sample by Luk Smolders. thanx!

---
+++ Hydrogene/Java/Android/Torch/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿//Sample app by Brian Long (http://blong.com)
+
+/*
+  This example demonstrates a number of things:
+  - how to invoke a confirmation dialog
+  - how to make a 'toast' message
+  - how to launch another activity
+  - how to make a full screen activity (no status bar)
+  - how to remove the activity title bar
+  - how to use a wake lock to prevent the screen from sleeping
+  - how to set up an options menu (one that shows when you press the Menu button)
+  - how to set up a context menu (one that shows after a long press)
+*/
+
+using java.util;
+using android.app;
+using android.content;
+using android.os;
+using android.util;
+using android.view;
+using android.widget;
+
+namespace org.me.torch
+{
+	public class MainActivity: Activity
+	{
+    private const int CONFIRM_TORCH_DIALOG = 1;    
+
+		public override void onCreate(Bundle savedInstanceState)
+		{
+			base.onCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			ContentView = R.layout.main;
+      // Get our button from the layout resource,
+      // and attach an event to it
+      var torchButton = findViewById(R.id.torchButton) as Button;
+      //torchButton.OnClickListener := method begin ShowDialog(CONFIRM_TORCH_DIALOG); end;
+      torchButton.OnClickListener = delegate(View v){showDialog(CONFIRM_TORCH_DIALOG);};        
+		}
+
+    public override Dialog onCreateDialog(int id)
+    {
+      if (id == CONFIRM_TORCH_DIALOG)
+      {
+        //Create dialog here and return it
+        //Note this is called just once regardless of number of invocations, unless you call RemoveDialog()
+        var builder = new AlertDialog.Builder(this);
+        builder.Message = R.@string.torchDialog_Message;      
+        builder.setPositiveButton(android.R.@string.yes, TorchDialog_YesClickedEvent);
+        builder.setNegativeButton(android.R.@string.no, delegate(DialogInterface dialog, int which){dialog.cancel();});      
+        return builder.create();
+      }
+      return base.onCreateDialog(id);
+    }
+    public void TorchDialog_YesClickedEvent(DialogInterface dialog, int which)
+    {
+      //Brief popup message aka toast
+      Toast.makeText(this, R.@string.torchButton_Toast, Toast.LENGTH_SHORT).show();
+      //Now show the torch screen  
+      var i = new Intent(this, typeOf(TorchActivity));
+      startActivity(i);
+    }
+	}
+}
---
+++ Hydrogene/Java/Android/Torch/TorchActivity.cs
@@ -0,0 +1,86 @@
+﻿using java.util;
+using android.app;
+using android.content;
+using android.os;
+using android.util;
+using android.view;
+using android.widget;
+
+namespace org.me.torch
+{
+	public class TorchActivity : Activity
+	{
+    public const int SWITCH_OFF_TORCH_ID = 1;
+    PowerManager mPowerManager;
+    PowerManager.WakeLock mWakeLock;
+		public override void onCreate(Bundle savedInstanceState)
+		{
+			base.onCreate(savedInstanceState);
+	    
+		  var mainLayout = new LinearLayout(this);
+      mainLayout.LayoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT);	  
+      //The torch is white
+      mainLayout.BackgroundColor = (Int64) 0xFFFFFFFF;
+      //Hide the regular activity title
+      requestWindowFeature(Window.FEATURE_NO_TITLE);
+      //Hide the OS status bar
+      Window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      //ensure this activity has full brightness
+      Window.Attributes.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_FULL;
+      // Get an instance of the PowerManager
+      mPowerManager = (PowerManager)SystemService[POWER_SERVICE];
+      // Create a bright wake lock. Requires WAKE_LOCK permission - see Android manifest file
+      mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, @Class.Name);
+      //Show the view
+      ContentView = mainLayout;
+      registerForContextMenu(mainLayout);
+		}
+    public override void onResume()
+    {
+      base.onResume();
+      // Acquire wake lock to keep screen on
+      mWakeLock.acquire();
+    }
+    public override void onPause()
+    {
+      base.onPause();
+      // Release wake lock to allow screen to turn off, as per normal
+      mWakeLock.release();
+    }
+
+    public override bool onCreateOptionsMenu(Menu menu)
+    {
+      var item = menu.add(0, SWITCH_OFF_TORCH_ID, 0, R.@string.torchMenuItem_Text);
+      //Options menu items support icons
+      item.Icon = android.R.drawable.ic_menu_close_clear_cancel;
+      return true;
+    }
+
+    public override void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+      base.onCreateContextMenu(menu, v, menuInfo);
+      menu.add(0, SWITCH_OFF_TORCH_ID, 0, R.@string.torchMenuItem_Text);
+    }
+
+    public bool MenuItemSelected(MenuItem item)
+    {
+      if (item.ItemId == SWITCH_OFF_TORCH_ID)
+      {
+        finish();
+        return true;
+      }
+      return false;
+    }
+
+    public override bool onContextItemSelected(MenuItem item)
+    {
+      return MenuItemSelected(item);
+    }
+
+    public override bool onOptionsItemSelected(MenuItem item)
+    {
+      return MenuItemSelected(item);
+    }
+
+	}
+}
